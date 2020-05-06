c646ed2ab6944453e1d0c466110e7dd74e37f8f6
Jose Irigon <irigon@gmail.com>
2020-05-06 15:09:03 +0200

    one.sh adding gson lib

--- one.sh
+++ one.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-java -Xmx512M -cp target:lib/ECLA.jar:lib/DTNConsoleConnection.jar core.DTNSim $*
+_JAVA_OPTIONS="-Xms8G -Xmx8G" java -XX:+FlightRecorder  -XX:StartFlightRecording=duration=60s,filename=myrecording.jfr -Djava.util.Arrays.useLegacyMergeSort=true -cp target:lib/ECLA.jar:lib/DTNConsoleConnection.jar:lib/gson-2.8.5.jar:lib/commons-math3-3.2.jar core.DTNSim $*
